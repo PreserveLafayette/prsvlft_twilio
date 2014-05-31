af4d41de6fd47f65d129d7b194b64b8d1b3e24c2
William Zhang <william_zhang@schumachergroup.com>
2014-05-31 11:37:19 -0500

    Updated the process.

--- web.rb
+++ web.rb
@@ -3,14 +3,44 @@ require 'twilio-ruby'
 require 'sinatra'
  
 get '/' do
-    'Hello World! Currently running version ' + Twilio::VERSION + \
-        ' of the twilio-ruby library.'
-end
-
-get '/hello-twilio' do
 	content_type 'text/xml'
 
 	Twilio::TwiML::Response.new do |r|
-		r.Say 'Floozy Flub Buddies'
+		r.Say "Welcome to Acadiana Open Channel."
+		r.Say "This is a community provided service used to gather stories from those around Acadiana."
+		r.Say "If you haven't already, please visit our website at www.floozy.com to start your conversation."
+		r.Gather :numDigits => '6', :action => '/gather', :method => 'get' do |g|
+			g.Say 'Please enter your 6 digit code presented on our website.'
+		end
+	end.text
+end
+
+get '/gather' do
+	content_type 'text/xml'
+	recognized = true
+	if recognized
+		Twilio::TwiML::Response.new do |r|
+			r.Say "Thanks. We recognize this location."
+			r.Say "Please leave your message after the tone."
+			r.Record :maxLength => '120', :action => '/record', :method => 'get'
+		end.text
+	else
+		Twilio::TwiML::Response.new do |r|
+			r.Say "We did not recognize this number.  Please try again."
+			r.Gather :numDigits => '6', :action => '/gather', :method => 'get' do |g|
+				g.Say 'Please enter your 6 digit code presented on our website.'
+			end
+		end.text
+	end
+end
+
+get '/record' do
+	content_type 'text/xml'
+
+	Twilio::TwiML::Response.new do |r|
+		r.Say "Thanks for your recording."
+		r.Say "This is what you said."
+		r.Play params['RecordingUrl']
+		r.Say "Thanks again for your contribution.  Please have a nice day."
 	end.text
 end
